5f5969f2ce4c672d7565fc0e681204d3dbdc09d2
Lam <ngonl@garena.com>
2021-07-05 21:50:20 +0800

    UPD: Product qty controller

--- src/components/ProductQuantityController/ProductQuantityController.jsx
+++ src/components/ProductQuantityController/ProductQuantityController.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import * as S from './productQuantityController.style';
 
 export default function ProductQuantityController() {
-  return <div></div>;
+  return (
+    <S.ProductQuantityController>
+      <S.ProductQuantityButton>
+        <svg enableBackground="new 0 0 10 10" viewBox="0 0 10 10" x={0} y={0} className="shopee-svg-icon ">
+          <polygon points="4.5 4.5 3.5 4.5 0 4.5 0 5.5 3.5 5.5 4.5 5.5 10 5.5 10 4.5" />
+        </svg>
+      </S.ProductQuantityButton>
+      <S.ProductQuantityInput />
+      <S.ProductQuantityButton>
+        <svg
+          enableBackground="new 0 0 10 10"
+          viewBox="0 0 10 10"
+          x={0}
+          y={0}
+          className="shopee-svg-icon icon-plus-sign"
+        >
+          <polygon points="10 4.5 5.5 4.5 5.5 0 4.5 0 4.5 4.5 0 4.5 0 5.5 4.5 5.5 4.5 10 5.5 10 5.5 5.5 10 5.5" />
+        </svg>
+      </S.ProductQuantityButton>
+    </S.ProductQuantityController>
+  );
 }
